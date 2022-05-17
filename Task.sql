8e9bc95428f5897a0faefb90ad4e1f449d7a043d
Ivan0305 <ivanchan0305@gmail.com>
2022-05-17 15:48:01 +0200

    second commit

--- Task.sql
+++ Task.sql
@@ -1,6 +1,6 @@
 ## Task 1: Formulieren Sie folgende Anfragen auf dem bekannten Universitätsschema in SQL. Geben Sie alle Ergebnisse duplikatfrei aus.
 
-# 1. (a)  Finden Sie die Studenten, die Sokrates aus Vorlesung(en) kennen.
+# 1. Finden Sie die Studenten, die Sokrates aus Vorlesung(en) kennen.
 select distinct s.Name, s.MatrNr
 from Studenten s, hoeren h, Vorlesungen v, Professoren p
 where s.Matrnr=h.Matrnr and h.VorlNr=v.VorlNr 
